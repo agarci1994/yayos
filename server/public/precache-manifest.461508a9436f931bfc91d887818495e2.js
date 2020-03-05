2f71b475e651e9fd035bc61f6d39d501a3899ad9
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-03-05 12:09:46 +0100

    second upload

---
+++ server/public/precache-manifest.461508a9436f931bfc91d887818495e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5962d0490a64de4ad979fe5f1f913a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97144f0a82ec9909ac4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "609bcf0e538bd9abf60c",
+    "url": "/static/css/main.d1f6edcd.chunk.css"
+  },
+  {
+    "revision": "e97144f0a82ec9909ac4",
+    "url": "/static/js/2.973dd204.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.973dd204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609bcf0e538bd9abf60c",
+    "url": "/static/js/main.226530a8.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "f8247c3949b8c8cd2d314ca146d6aefb",
+    "url": "/static/media/7b658101a5fe408594d579077d037a52.f8247c39.jpg"
+  }
+]);
