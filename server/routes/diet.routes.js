82983413442c5f6fa359b12197d499b4b54ea1c3
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-03-05 12:56:00 +0100

    api2

--- server/routes/diet.routes.js
+++ server/routes/diet.routes.js
@@ -16,38 +16,41 @@ router.post('/new', (req, res, next) => {
 
 router.get('/main', (req, res, next) => {
     console.log("aqui si que entra")
-    searchDiet.getRecipe(req.user.diet)
-        .then(recipe => {
-            let recipes = []
-            for (let i = 0; i < 7; i++) {
-                let breakfast = Math.floor(Math.random() * recipe.data.hits.length)
-                let lunch = Math.floor(Math.random() * recipe.data.hits.length)
-                let dinner = Math.floor(Math.random() * recipe.data.hits.length)
-                let day = {
-                    [i]: {
-                        breakfast: {
-                            name: recipe.data.hits[breakfast].recipe.label,
-                            ingredients: recipe.data.hits[breakfast].recipe.ingredientLines
-                        },
-                        lunch: {
-                            name: recipe.data.hits[lunch].recipe.label,
-                            ingredients: recipe.data.hits[lunch].recipe.ingredientLines
-                        },
-                        dinner: {
-                            name: recipe.data.hits[dinner].recipe.label,
-                            ingredients: recipe.data.hits[dinner].recipe.ingredientLines
-                        }
-                    }
-                }
-                recipes.push(day)
-            }
-            return User.findByIdAndUpdate(req.user._id, {
-                recipe: recipes
-            })
+    res.json({
+        msg: "funciona"
+    })
+    // searchDiet.getRecipe(req.user.diet)
+    //     .then(recipe => {
+    //         let recipes = []
+    //         for (let i = 0; i < 7; i++) {
+    //             let breakfast = Math.floor(Math.random() * recipe.data.hits.length)
+    //             let lunch = Math.floor(Math.random() * recipe.data.hits.length)
+    //             let dinner = Math.floor(Math.random() * recipe.data.hits.length)
+    //             let day = {
+    //                 [i]: {
+    //                     breakfast: {
+    //                         name: recipe.data.hits[breakfast].recipe.label,
+    //                         ingredients: recipe.data.hits[breakfast].recipe.ingredientLines
+    //                     },
+    //                     lunch: {
+    //                         name: recipe.data.hits[lunch].recipe.label,
+    //                         ingredients: recipe.data.hits[lunch].recipe.ingredientLines
+    //                     },
+    //                     dinner: {
+    //                         name: recipe.data.hits[dinner].recipe.label,
+    //                         ingredients: recipe.data.hits[dinner].recipe.ingredientLines
+    //                     }
+    //                 }
+    //             }
+    //             recipes.push(day)
+    //         }
+    //         return User.findByIdAndUpdate(req.user._id, {
+    //             recipe: recipes
+    //         })
 
-        })
-        .then(x => console.log(x))
-        .catch(err => console.log(err))
+    //     })
+    //     .then(x => console.log(x))
+    //     .catch(err => console.log(err))
 })
 
 module.exports = router;
