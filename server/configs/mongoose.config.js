00a04e5029a8b9f25af857e3b41b9ca5a14f9806
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-03-05 12:24:14 +0100

    upload

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(process.env.DB_LOCAL, {
+    .connect(process.env.DB_SERVER, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
